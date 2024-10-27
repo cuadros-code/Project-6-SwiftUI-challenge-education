9e39ecb965a313c9b2d26b60c277f1edc5a7a3eb
kevin cuadros <kevindavidcuadros@gmail.com>
2024-10-26 21:29:25 -0500

    create app

--- Project-6-SwiftUI-challenge-education/ContentView.swift
+++ Project-6-SwiftUI-challenge-education/ContentView.swift
@@ -9,16 +9,45 @@ import SwiftUI
 
 struct ContentView: View {
     
-    
+    @State private var selectedTable = 2
+    @State private var selectedNumberOfQuestions = 5
     
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Form {
+            Section("Numbers of questions") {
+                Picker("Numbers of questions", selection: $selectedNumberOfQuestions) {
+                    ForEach(2..<21, id: \.self){
+                        if $0 % 5 == 0 {
+                            Text("\($0)")
+                        }
+                    }
+                }
+                .pickerStyle(.segmented)
+            }
+            
+            Section {
+                Picker("Select multiplication table", selection: $selectedTable) {
+                    ForEach(2..<13, id: \.self){
+                        Text("\($0)")
+                    }
+                }
+                .pickerStyle(.inline)
+            }
+            
+            Section {
+                HStack {
+                    Spacer()
+                    Button("Start"){
+                        
+                    }
+                    .background(.indigo)
+                    .foregroundStyle(.white)
+                    .padding()
+                    Spacer()
+                }
+            }
         }
-        .padding()
+        
     }
 }
 
