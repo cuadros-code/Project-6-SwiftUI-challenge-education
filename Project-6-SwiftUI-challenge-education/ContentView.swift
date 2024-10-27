448574d1179ffc0b6a8e4926684a63dbe18aee79
kevin cuadros <kevindavidcuadros@gmail.com>
2024-10-26 21:01:10 -0500

    Initial Commit

---
+++ Project-6-SwiftUI-challenge-education/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  Project-6-SwiftUI-challenge-education
+//
+//  Created by Kevin Cuadros on 26/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    
+    
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Project-6-SwiftUI-challenge-education/Project_6_SwiftUI_challenge_educationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_6_SwiftUI_challenge_educationApp.swift
+//  Project-6-SwiftUI-challenge-education
+//
+//  Created by Kevin Cuadros on 26/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Project_6_SwiftUI_challenge_educationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
